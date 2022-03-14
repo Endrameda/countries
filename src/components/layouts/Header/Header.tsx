5ef8e99a766de3e88ee90da8b2aaf73331e6a437
Sardor <Endrameda98@gmail.com>
2022-03-14 18:48:47 +0500

    Fix

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {ThemeButton} from "../../ThemeButton/ThemeButton";
 
 import './header.css'
+import {Link} from "react-router-dom";
 
 export const Header = () => {
     return (
         <header className="header shadow-md shadow-gray-200 dark:shadow-dark-blue-lmt mb-12">
             <div className="header__container py-6 px-2.5">
-                <h1 className="text-[20px] sm:text-2xl font-extrabold text-dark-blue-lmt dark:text-white">
+                <Link to={'/'}><h1
+                    className="text-[20px] sm:text-2xl font-extrabold text-dark-blue-lmt dark:text-white">
                     Where in the world?
                 </h1>
+                </Link>
                 <ThemeButton/>
             </div>
         </header>
